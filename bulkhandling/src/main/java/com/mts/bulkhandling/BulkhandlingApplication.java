60c0f4a1cc8a7a0ef7f32be8cefb5f584b30d235
amr.boghdady <amr@gmail.com>
2026-03-12 11:57:10 +0200

    Initiate project

---
+++ bulkhandling/src/main/java/com/mts/bulkhandling/BulkhandlingApplication.java
@@ -0,0 +1,13 @@
+package com.mts.bulkhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkhandlingApplication.class, args);
+	}
+
+}
---
+++ bulkhandling/src/test/java/com/mts/bulkhandling/BulkhandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mts.bulkhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkhandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
